8cc2f87f1d2b666a9f736945ab559b8e726f163e
flaviowilker <flaviow.wilker@gmail.com>
2020-09-29 23:33:56 -0300

    newrole function

--- domain/role.go
+++ domain/role.go
@@ -8,3 +8,14 @@ type Role struct {
 	Name string `gorm:"type:varchar(255);unique"`
 	Code string `gorm:"type:varchar(255);unique"`
 }
+
+// NewRole ...
+func NewRole(name string, code string) (*Role, error) {
+
+	role := &Role{
+		Name: name,
+		Code: code,
+	}
+
+	return role, nil
+}
