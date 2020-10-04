df996611a4a0a22bf770fa1b5f5b6d891d79b70f
flaviowilker <flaviow.wilker@gmail.com>
2020-10-04 00:23:34 -0300

    database config

--- app/infrastructure/cmd/server/main.go
+++ app/infrastructure/cmd/server/main.go
@@ -1,16 +1,22 @@
 package main
 
 import (
+	"log"
+
 	"github.com/flaviowilker/rentcar/app/infrastructure/database"
 	"github.com/flaviowilker/rentcar/app/infrastructure/web"
 )
 
 func main() {
 	db := database.NewConfig()
-	dbSQL, err := db.DB()
+	sqlDB, err := db.DB()
 	if err != nil {
-		defer dbSQL.Close()
+		log.Fatal(err)
+		panic(err)
 	}
+	sqlDB.SetMaxIdleConns(0)
+	sqlDB.SetMaxOpenConns(1)
+	defer sqlDB.Close()
 
 	web.CreateConfig(db)
 }
